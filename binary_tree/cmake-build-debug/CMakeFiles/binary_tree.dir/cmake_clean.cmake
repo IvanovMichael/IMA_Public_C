799d9556cf57b68dd8ef91ab1235477d83cc84fe
IvanovMichael <37982890+IvanovMichael@users.noreply.github.com>
2019-05-21 23:16:49 +0300

    Educational
    Realization of binary tree with function FIND

---
+++ binary_tree/cmake-build-debug/CMakeFiles/binary_tree.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/binary_tree.dir/main.c.o"
+  "CMakeFiles/binary_tree.dir/tree.c.o"
+  "binary_tree.pdb"
+  "binary_tree"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/binary_tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
